a20aef230230ce4526c52672b7de24d5b275a95b
Ilya Semak <ladysemak2001@gmail.com>
2024-08-21 17:45:59 +0300

    homework_15_1_1

--- src/classes.py
+++ src/classes.py
@@ -53,7 +53,8 @@ class Category:
 
     def __str__(self):
         """Возвращает строковое представление экземпляра класса Category."""
-        return f"{self.name}, количество продуктов: {Category.product_count} шт."
+        sum_product = sum([product.quantity for product in self.__products])
+        return f"{self.name}, количество продуктов: {sum_product} шт."
 
     def add_product(self, products):
         """Добавляет новый продукт в категорию."""
@@ -72,3 +73,28 @@ class Category:
     def get_all_products(self):
         """Возвращает список объектов Product."""
         return self.__products
+
+
+
+
+product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+print(str(product1))
+print(str(product2))
+print(str(product3))
+
+category1 = Category(
+    "Смартфоны",
+    "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+    [product1, product2, product3]
+)
+
+print(str(category1))
+
+print(category1.products)
+
+print(product1 + product2)
+print(product1 + product3)
+print(product2 + product3)
